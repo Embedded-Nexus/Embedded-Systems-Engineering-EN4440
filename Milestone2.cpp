77439abcac2828aed80d1e9fa2ec27815155fcfa
RuchiraAbeywardhane <ruchiraabeywardhane08@gmail.com>
2025-09-07 09:50:23 +0530

    intital function definitions

---
+++ Milestone2.cpp
@@ -0,0 +1,21 @@
+void BuildReadFrame(){}
+void SendReadFrame(){}
+
+void extractFrame(){}
+void CRCCheck(){}
+void decoder(){}
+
+void ReadDataFrame(){
+    extractFrame();
+    CRCCheck();
+    decoder();
+}
+
+
+void Scheduler(){}
+
+void BuildWriteFrame(){}
+void WriteandVerify(){}
+void errorPolicy(){}
+
+void demo(){}
